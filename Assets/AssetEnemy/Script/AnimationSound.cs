9f3f859c6386865a2f1bc9210fb7127cbed8a37b
KhoaNguyenViet <vkhoa2296@gmail.com>
2025-06-03 20:40:44 +0700

    update animation sound script

--- Assets/AssetEnemy/Script/AnimationSound.cs
+++ Assets/AssetEnemy/Script/AnimationSound.cs
@@ -1,19 +1,43 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnimationSound : MonoBehaviour
 {
     [SerializeField] private AudioSource _local;
+    [SerializeField] private Transform _player;
+    [SerializeField] private float maxDistance = 10f ;
 
-    public void PlayHitSound(string name)
+    private void Start()
     {
         if (_local != null)
         {
-            var clip = AudioManager.Instance.GetHitClip(name);
-            if(clip != null) _local.PlayOneShot(clip);
+            _local.spatialBlend = 1f; // Bật chế độ 3D
+            _local.minDistance = 2f;  // Nghe rõ trong khoảng này
+            _local.maxDistance = 10f; // Âm thanh mờ dần sau khoảng này
+            _local.rolloffMode = AudioRolloffMode.Linear; // Kiểu giảm âm
         }
-        else
+    }
+    public void PlayHitSound(string name)
+    {
+        if(_player == null)
         {
-            AudioManager.Instance.PlayHitSound(name);
+            _player = GameObject.FindGameObjectWithTag("Player").transform;
+            if(_player == null)  return;
         }
+
+        float distance = Vector3.Distance(transform.position, _player.position);
+
+        if (distance <= maxDistance)
+        {
+            if (_local != null)
+            {
+                var clip = AudioManager.Instance.GetHitClip(name);
+                if (clip != null) _local.PlayOneShot(clip);
+            }
+            else
+            {
+                AudioManager.Instance.PlayHitSound(name);
+            }
+        }
+        
     }
 }
