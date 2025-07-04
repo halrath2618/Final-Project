4ab613c073d50a055f2757ee62a971509f317358
halrath2618 <158588123+halrath2618@users.noreply.github.com>
2025-07-04 13:02:08 +0700

    Update Tail Animation

--- Assets/Scripts/GUI/VolumeController.cs
+++ Assets/Scripts/GUI/VolumeController.cs
@@ -1,27 +1,27 @@
-using UnityEngine;
-using UnityEngine.UI;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class VolumeController : MonoBehaviour
-{
+//public class VolumeController : MonoBehaviour
+//{
 
-    [SerializeField] private Scrollbar masterVolume;
-    [SerializeField] private Scrollbar musicVolume;
-    [SerializeField] private Scrollbar sfxVolume;
+//    [SerializeField] private Scrollbar masterVolume;
+//    [SerializeField] private Scrollbar musicVolume;
+//    [SerializeField] private Scrollbar sfxVolume;
 
-    private AudioSource masterAudioSource;
-    private AudioSource musicAudioSource;
-    private AudioSource sfxAudioSource;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+//    private AudioSource masterAudioSource;
+//    private AudioSource musicAudioSource;
+//    private AudioSource sfxAudioSource;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        masterVolume.value = masterAudioSource.volume;
-        musicVolume.value = musicAudioSource.volume;
-        sfxVolume.value = sfxAudioSource.volume;
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        masterVolume.value = masterAudioSource.volume;
+//        musicVolume.value = musicAudioSource.volume;
+//        sfxVolume.value = sfxAudioSource.volume;
+//    }
+//}
